0afd718f46d7c2fb120b101cf9a688a5ca80303f
Albin Odervall <albin.odervall@gmail.com>
2013-05-07 15:30:31 +0200

    Add Array.prototype.shuffle function

---
+++ src/scripts/library/array.shuffle.js
@@ -0,0 +1,5 @@
+Array.prototype.shuffle = function(o) {
+    'use strict';
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
