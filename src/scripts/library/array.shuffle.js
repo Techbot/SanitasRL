5c5f55fd4313eb42a41cce753b89bea55c079dd1
Albin Odervall <albin.odervall@gmail.com>
2013-05-07 15:32:14 +0200

    Move Array.prototype.shuffle to Array.shuffle

--- src/scripts/library/array.shuffle.js
+++ src/scripts/library/array.shuffle.js
@@ -1,4 +1,4 @@
-Array.prototype.shuffle = function(o) {
+Array.shuffle = function(o) {
     'use strict';
     for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
